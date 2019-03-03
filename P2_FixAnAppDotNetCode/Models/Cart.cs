e098a5abf52c14293886618f284ac25d5e686db0
Jeremy Willhelm <46633603+CodeslayerJay@users.noreply.github.com>
2019-03-03 17:46:24 -0500

    fixed AddItem

--- P2_FixAnAppDotNetCode/Models/Cart.cs
+++ P2_FixAnAppDotNetCode/Models/Cart.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using Microsoft.WindowsAzure.Storage.File.Protocol;
 
 namespace P2_FixAnAppDotNetCode.Models
 {
@@ -13,21 +15,53 @@ namespace P2_FixAnAppDotNetCode.Models
         /// </summary>
         public IEnumerable<CartLine> Lines => GetCartLineList();
 
+        // Create a private empty list
+        private List<CartLine> _lines = new List<CartLine>();
+
         /// <summary>
         /// Return the actual cartline list
         /// </summary>
         /// <returns></returns>
         private List<CartLine> GetCartLineList()
         {
-            return new List<CartLine>();
+
+            // Every time method is called a "new" empty List<CartLine>() is returned
+            //return new List<CartLine>();
+
+            // We want to persist the collection so we create a class member and initialize it to
+            // new empty List<CartLine>();
+            // Now when method is called we return the member collection
+            return _lines;
         }
 
         /// <summary>
         /// Adds a product in the cart or increment its quantity in the cart if already added
-        /// </summary>//
+        /// </summary>
         public void AddItem(Product product, int quantity)
         {
-            // TODO implement the method
+            // Get product from _lines collection
+            CartLine line = _lines.Find(l => l.Product.Id == product.Id);
+
+            // If: line already has a product -> update the quantity
+            // Else: Create a new item and store in the collection.
+            if (line != null)
+            {
+                line.Quantity = line.Quantity + 1;
+            }
+            else
+            {
+                // Create an OrderLineID based on the current count of _lines
+                int OrderId = 0;
+                int count = _lines.Count;
+                OrderId = count + 1;
+
+                // Create new CartLine object passing in our arguments
+                CartLine cartLine = new CartLine() { OrderLineID = OrderId, Product = product, Quantity = quantity };
+
+                // Use LINQ to Add to list
+                _lines.Add(cartLine);
+            }
+            
         }
 
         /// <summary>
@@ -60,6 +94,8 @@ namespace P2_FixAnAppDotNetCode.Models
         public Product FindProductInCartLines(int productId)
         {
             // TODO implement the method
+            
+            
             return null;
         }
 
